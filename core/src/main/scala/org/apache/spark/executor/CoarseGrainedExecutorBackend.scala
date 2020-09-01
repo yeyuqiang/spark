31678c038e78c856636c05af646ac4d9b2957d85
haojinIntel <hao.jin@intel.com>
2020-09-01 10:59:33 +0800

    Add patch for RDD cache

--- core/src/main/scala/org/apache/spark/executor/CoarseGrainedExecutorBackend.scala
+++ core/src/main/scala/org/apache/spark/executor/CoarseGrainedExecutorBackend.scala
@@ -324,6 +324,7 @@ private[spark] object CoarseGrainedExecutorBackend extends Logging {
           driverConf.set(key, value)
         }
       }
+      driverConf.set("spark.executor.numa.id", s"${numaNodeId.getOrElse(-1)}")
 
       cfg.hadoopDelegationCreds.foreach { tokens =>
         SparkHadoopUtil.get.addDelegationTokens(tokens, driverConf)
