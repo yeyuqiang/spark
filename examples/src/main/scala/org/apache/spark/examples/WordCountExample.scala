74e413e39ae42d3d584f97aef5105188e56b5237
yeyuqiang <yuqiang.ye@intel.com>
2021-03-02 15:06:46 +0800

    [POAE7-909] Add a debuggable example for plasma shuffle backend (#69)
    * Make it easy to debug with a word count example
    
    * config plasma shuffle backend in word count example

---
+++ examples/src/main/scala/org/apache/spark/examples/WordCountExample.scala
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.examples
+
+import org.apache.spark.sql.SparkSession
+
+object WordCountExample {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder()
+      .appName("Spark Word Count Example")
+      .config("spark.shuffle.plasma.enabled", true)
+      .config(
+        "spark.shuffle.sort.io.plugin.class",
+        "org.apache.spark.shuffle.pmem.PlasmaShuffleDataIO"
+      )
+      .getOrCreate()
+
+    import spark.sqlContext.implicits._
+
+    val textFile = spark.read.textFile("examples/src/main/resources/people.txt")
+    val wordCounts = textFile.flatMap(line => line.split(" ")).groupByKey(identity).count()
+    val result = wordCounts.collect()
+
+    // scalastyle:off println
+    println(result.toSeq.mkString(" "))
+    // scalastyle:off println
+
+    spark.stop()
+  }
+
+}
